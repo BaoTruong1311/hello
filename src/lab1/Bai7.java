3766571c2a753a477506a527ad815ac25f7a6226
BaoTruong1311 <18130262@st.hcmuaf.edu.vn>
2021-03-19 09:22:55 +0700

    Vi du 1

---
+++ src/lab1/Bai7.java
@@ -0,0 +1,17 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Bai7 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Nhap vao xA yA xB yB: ");
+		double xA = input.nextDouble();
+		double yA = input.nextDouble();
+		double xB = input.nextDouble();
+		double yB = input.nextDouble();
+		double d = Math.sqrt((xA - xB) * (xA - xB) + (yA - yB) * (yA - yB));
+		System.out.println("khoảng cách d giữa 2 điểm a va B la: " + d);
+
+	}
+}
